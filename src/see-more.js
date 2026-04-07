eefb6d62a07f02bd10c5db74296407a2fd561e30
GosseFlorian <floriangosse91@gmail.com>
2026-04-07 11:13:48 +0200

    test du button fonctionnelle et mise en commentaire des possible description supplementaire

--- src/see-more.js
+++ src/see-more.js
@@ -1,5 +1,10 @@
 import "./style.css";
 
-export function seeMore(){
-    console.log("zut")
+export function seeMore() {
+  console.log("zut");
+
+  //     <div class="see-more">
+  //     <p>Date de début : ${place.date_debut}</p>
+  //     <p>Date de fin : ${place.date_fin}</p>
+  // </div>
 }
