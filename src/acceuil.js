22d9e5bc868c126f1ababaa1171089da29369f82
GosseFlorian <floriangosse91@gmail.com>
2026-04-07 10:55:49 +0200

    ajout d'un titre et d'un statut de recherche, creation de la fonction de recherche par mot clé et debut de mise en place du button see-more

--- src/acceuil.js
+++ src/acceuil.js
@@ -3,9 +3,27 @@ import { searchData } from "./search-data";
 
 export function acceuilDom() {
   document.querySelector("#app").innerHTML = `
+    <h1>Lieux de tournage à Paris</h1>
     <input type="text" id="recherche" placeholder="Tapez pour rechercher..." />
+    <p id="statut">Entrez un mot-clé pour commencer.</p>
       <div id="resultats"></div>
     `;
-    const inputSearch = document.getElementById("recherche");
-    inputSearch.addEventListener("input", searchData)
+  const inputSearch = document.getElementById("recherche");
+  const resultats = document.getElementById("resultats");
+  const statut = document.getElementById("statut");
+  let debounce = null;
+
+  inputSearch.addEventListener("input", () => {
+    let value = inputSearch.value;
+    clearTimeout(debounce);
+
+    if (value.length === 0) {
+      statut.textContent = "Entrez un mot-clé pour commencer."
+      resultats.innerHTML = "";
+      return;
+    }
+    debounce = setTimeout(function () {
+      searchData(value);
+    }, 500);
+  });
 }
--- src/search-data.js
+++ src/search-data.js
@@ -1,5 +1,53 @@
-import './style.css'
+import "./style.css";
+import { seeMore } from "./see-more";
 
-export function searchData() {
-    console.log("OK")
+export async function searchData(keyword) {
+  const statut = document.getElementById("statut");
+  const resultats = document.getElementById("resultats");
+  let controller = null;
+
+  if (controller) {
+    controller.abort();
+  }
+  controller = new AbortController();
+  resultats.innerHTML = "";
+
+  try {
+    const response = await fetch("https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?limit=100", {
+      signal: controller.signal,
+    });
+    const places = await response.json();
+
+    let filtres = places.results.filter((place) => {
+      return place.nom_tournage.toLowerCase().includes(keyword.toLowerCase());
+    });
+    let tenFiltres = filtres.slice(0, 10);
+
+    if (tenFiltres.length === 0) {
+      statut.textContent = `${tenFiltres.length} resultat`;
+    } else {
+      statut.textContent = `${tenFiltres.length} resultats`;
+      for (let place of tenFiltres) {
+        const div = document.createElement("div");
+        div.className = "place";
+        div.innerHTML = `
+                    <h3>Title : ${place.nom_tournage}</h3>
+                    <h4>Type de tournage : ${place.type_tournage}</h4>
+                    <p>Lieu : ${place.adresse_lieu}</p>
+                    <button class="button">See more</button>
+                    <div class="see-more">
+                        <p>Date de début : ${place.date_debut}</p>
+                        <p>Date de fin : ${place.date_fin}</p>
+                    </div>
+                    `;
+        resultats.appendChild(div);
+      }
+    }
+  } catch (err) {
+    if (err.name === "AbortError") return;
+    statut.textContent = "Erreur API";
+  }
+
+//   const button = document.querySelector("button")
+//   button.addEventListener("click", seeMore())
 }
