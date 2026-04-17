055c8fad147f9c8697f46b9851fa81ec744a95b3
GosseFlorian <floriangosse91@gmail.com>
2026-04-17 11:12:33 +0200

    ajout du fonctionnement du button pour montrer/cacher du contenu

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react'
 import './card.css'
 
-export function Card({titre, lieu, realisateur, onToggle}) {
+export function Card({titre, lieu, realisateur}) {
+  const [isOpen, setIsOpen] = useState(false)
 
+  function handleToggle() {
+    setIsOpen(!isOpen)
+  }
+  
   return (
     <div className="card">
       <h2>Title : {titre}</h2>
       <h3>Lieu : {lieu}</h3>
-      <p className="more-desc hide">Realisateur : {realisateur}</p>
-      <button className="button" onToggle={onToggle}>See more</button>
+      <p className={isOpen ? "more-desc" : "more-desc hide"}>
+        Realisateur : {realisateur}
+      </p>
+      <button className="button" onClick={handleToggle}>
+        {isOpen ? "See less" : "See more"}
+      </button>
     </div>
-  );
+  )
 }
